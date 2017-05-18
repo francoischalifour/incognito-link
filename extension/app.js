1a3260dd62a6ace31b92bea59961ef2e72bbcb7d
francoischalifour <francois.chalifour@gmail.com>
2017-05-18 19:26:18 +0100

    :books: Improve docs

--- extension/app.js
+++ extension/app.js
@@ -1,17 +1,17 @@
 /**
- * Goes up in the tree from a given node and
- * returns the first node being a link.
+ * Goes up in the nodelist until it finds a link and returns it.
  *
  * This is a recursive function to make sure to handle the
  * `href` attribute of the real link.
  *
+ * Example:
+ * <a href="#"><code>HTMLAnchorElement</code></a>
  *
- * Example: <a href="#"><em>Child node without `href`</em></a>
- * In this case, the `em` node doesn't have a `href` attribute
+ * In this case, the `code` node doesn't have a `href` attribute
  * so it needs to fall back to `a`.
  *
  * @param {object} node The node to get the link from
- * @returns {object} the first node having a `href` property
+ * @returns {object} the first node having a `href` property, `undefined` if none
  */
 const getLink = node => {
   if (!node) {
@@ -22,7 +22,7 @@ const getLink = node => {
 }
 
 /**
- * Handles the click event.
+ * Dispatches the action to event listeners in the background.
  *
  * Fired if the node is clicked pressing the correct key combinaison
  * and is a link.
@@ -43,13 +43,16 @@ const onClick = e => {
 
   e.preventDefault()
 
-  // Send the action to event listeners in `background.js`
   chrome.runtime.sendMessage({
     action: 'CREATE_INCOGNITO_WINDOW',
     url: link.href
   })
 }
 
+/*
+ * Binds the listener on the document object to intercept any links
+ * added in the future.
+ */
 document.addEventListener('DOMContentLoaded', () => {
   document.body.addEventListener('click', onClick)
 })
